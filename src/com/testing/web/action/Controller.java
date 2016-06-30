0242cb4f0227e0c99c9389691453317d4d5aec55
luoshaomu <15510515126@163.com>
2016-06-30 23:23:25 +0800

    首次创建项目

---
+++ src/com/testing/web/action/Controller.java
@@ -0,0 +1,10 @@
+package com.testing.web.action;
+
+public class Controller {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+	}
+
+}
